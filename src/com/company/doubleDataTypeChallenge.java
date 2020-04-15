ca5163cf7a5bfed7eb07169a83ee67400ba670a0
rahulcaet <63642672+rahulcaet@users.noreply.github.com>
2020-04-15 20:39:25 +0530

    Create doubleDataTypeChallenge.java
    exercise for decimal numbers

---
+++ src/com/company/doubleDataTypeChallenge.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class doubleDataTypeChallenge {
+    public static void main(String[] args) {
+        double poundVal = 4.5;
+        double kiloVal  = 0.45359237*poundVal;
+        System.out.println("pound to kilogram is:"+ kiloVal);
+
+    }
+}
