12ba87f89d5329ef17bdd9949bcd454f546450f4
rahulcaet <63642672+rahulcaet@users.noreply.github.com>
2020-04-27 21:04:17 +0530

    Add files via upload

---
+++ src/com/company/abstractclass/Parrot.java
@@ -0,0 +1,12 @@
+package com.company.abstractclass;
+
+public class Parrot extends Bird {
+    public Parrot(String name) {
+        super(name);
+    }
+
+//    @Override
+//    public void fly() {
+//        System.out.println("Parrot is flying....");
+//    }
+}
