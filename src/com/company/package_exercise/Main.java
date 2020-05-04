f3d754e88a3ff7c5666bc9dac883a06c0a1c2c8a
rahulcaet <63642672+rahulcaet@users.noreply.github.com>
2020-05-04 21:26:01 +0530

    Create Main.java

---
+++ src/com/company/package_exercise/Main.java
@@ -0,0 +1,11 @@
+package com.company.package_exercise;
+
+import com.example._package.Series;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Series.nSum(10).toString());
+        System.out.println(Series.factorial(10).toString());
+        System.out.println(Series.fibonacci(10).toString());
+    }
+}
