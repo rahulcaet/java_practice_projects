0cb84e0fcd011b3b94214787c8f257f8aca55516
rahulcaet <63642672+rahulcaet@users.noreply.github.com>
2020-05-04 21:24:53 +0530

    Create Main.java

---
+++ src/com/company/packages_scope_access_mod/Main.java
@@ -0,0 +1,13 @@
+package com.company.packages_scope_access_mod;
+
+import javax.xml.soap.Node;
+
+//import org.w3c.dom.Node;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Node node = null;
+        org.w3c.dom.Node node2 = null;
+    }
+}
